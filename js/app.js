b443cbd6e05a1e0c25d2df1135c99d2143db0899
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-02-16 22:38:01 +0600

    expenses calculate done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log(5);
+const calculateBtn = document.getElementById('calculate-btn');
+const saveBtn = document.getElementById('save-button')
+
+function getBalance() {
+    let salaryAmount = document.getElementById('salary-amount');
+    let foodAmount = document.getElementById('food-amount');
+
+    let rentAmount = document.getElementById('rent-amount');
+
+    let clothesAmount = document.getElementById('clothes-amount');
+    let totalSpent = parseFloat(foodAmount.value) + parseFloat(rentAmount.value) + parseFloat(clothesAmount.value);
+    let totalExpenses = document.getElementById('total-expenses').innerText = totalSpent;
+
+    let balance = document.getElementById('balance').innerText = salaryAmount.value - totalExpenses;
+
+}
+
+
+calculateBtn.addEventListener('click', function () {
+    getBalance();
+})
+
+saveBtn.addEventListener('click', function () {
+
+
+})
