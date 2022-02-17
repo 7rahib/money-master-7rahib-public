8eeb161a932410f5083cd190378e8a4809336271
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-02-17 20:24:31 +0600

    Error Handled and made responsive

--- js/app.js
+++ js/app.js
@@ -1,32 +1,88 @@
+// Buttons
 const calculateBtn = document.getElementById('calculate-btn');
 const saveBtn = document.getElementById('save-button');
+
+// Expenses
 const salaryAmount = document.getElementById('salary-amount');
 const foodAmount = document.getElementById('food-amount');
 const rentAmount = document.getElementById('rent-amount');
 const clothesAmount = document.getElementById('clothes-amount');
+const totalExpenses = document.getElementById('total-expenses');
 const balance = document.getElementById('balance');
-const totalExpenses = document.getElementById('total-expenses')
-const remainingBalance = document.getElementById('remaining-balance');
+
+
+// Savings
+const savingPercentage = document.getElementById('saving-percentage');
 const totalSaving = document.getElementById('total-saving');
-const savingPercentage = document.getElementById('saving-amount');
+const remainingBalance = document.getElementById('remaining-balance');
 
+
+// Expenses Error
+function expensesError() {
+    Swal.fire('Error!!! You can not spend more than you have');
+    foodAmount.value = '';
+    rentAmount.value = '';
+    clothesAmount.value = '';
+}
+
+// Saving Error
+function savingError() {
+    Swal.fire('Error!!! You can not save more than you have in your Balance');
+    savingPercentage.value = '';
+}
+
+// Handling Expenses
 function getBalance() {
-    let totalSpent = parseFloat(foodAmount.value) + parseFloat(rentAmount.value) + parseFloat(clothesAmount.value);
-    totalExpenses.innerText = totalSpent;
-    balance.innerText = parseFloat(salaryAmount.value) - parseFloat(totalSpent);
 
+    if (isNaN(salaryAmount.value) || isNaN(foodAmount.value) || isNaN(rentAmount.value) || isNaN(clothesAmount.value)) {
+        Swal.fire('Error!!! Please Enter Number');
+    }
+    else {
+        if (salaryAmount.value < 0 || foodAmount.value < 0 || rentAmount.value < 0 || clothesAmount.value < 0) {
+            Swal.fire('Error!!! Please Enter Positive Number');
+        }
+        else {
+            let totalSpent = parseFloat(foodAmount.value) + parseFloat(rentAmount.value) + parseFloat(clothesAmount.value);
+            if (totalSpent > salaryAmount.value) {
+                expensesError();
+            }
+            else {
+                totalExpenses.innerText = totalSpent;
+                balance.innerText = parseFloat(salaryAmount.value) - parseFloat(totalSpent);
+            }
+        }
+    }
 }
-function getSaved() {
+
+// Handling Savings
+function getSavedAmount() {
     getBalance();
-    totalSaving.innerText = (parseFloat(balance.innerText / 100)) * parseFloat(savingPercentage.value);
-    remainingBalance.innerText = parseFloat(balance.innerText) - parseFloat(totalSaving.innerText);
-
+    if (isNaN(savingPercentage.value)) {
+        Swal.fire('Error!! Please Enter Number')
+    }
+    else {
+        if (savingPercentage.value < 0) {
+            Swal.fire('Error!!! Please Enter Positive Number');
+        }
+        else {
+            let totalSaved = (parseFloat(salaryAmount.value / 100)) * parseFloat(savingPercentage.value);
+            if (totalSaved > balance.innerText) {
+                savingError();
+            }
+            else {
+                totalSaving.innerText = totalSaved;
+                remainingBalance.innerText = parseFloat(balance.innerText) - parseFloat(totalSaving.innerText);
+            }
+        }
+    }
 }
 
+// Calculating Expenses
 calculateBtn.addEventListener('click', function () {
     getBalance();
 })
 
+// Calculating Savings
 saveBtn.addEventListener('click', function () {
-    getSaved();
+    getSavedAmount();
 })
